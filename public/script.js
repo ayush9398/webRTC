a7aa863c67e0b83d91cfffe0ef30ef8cbc925524
Ayush Mittal <ayush.mittal9398@gmail.com>
2020-08-14 12:26:30 +0530

    files added

---
+++ public/script.js
@@ -0,0 +1,57 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const peer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+});
+
+const myvideo = document.createElement('video');
+myvideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myvideo, stream)
+
+    peer.on('call', call => {
+        call.answer(stream);
+        video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        connectMyPeer(userId, stream);
+    })
+})
+
+peer.on('open', id => {
+    socket.emit('join-room', room_id, id);
+})
+
+
+socket.on('user-connected', userId => {
+    console.log("user connected: "+ userId);
+})
+
+function addVideoStream(video, stream){
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video)
+}
+
+function connectMyPeer(userId, stream){
+    call = peer.call(userId, stream);
+    video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+
+    call.on('close', ()=>{
+        video.remove();
+    })
+}
